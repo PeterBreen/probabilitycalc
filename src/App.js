ca6be98abd282099e2b3ce45296430ad242859f2
Peter Breen <pbreen@gmail.com>
2017-12-07 13:52:17 -0800

    initial text

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is a tool for calculating the probability of getting a certain outcome at least once, within a given number of attempts.
+        </p>
+        <p className="App-intro">
+          For example, the probability of rolling a 3 on a 6-sided die is 16.67%. If you roll the die 25 times, the probability of rolling a 3 at least once is 98.95%.
         </p>
       </div>
-    );
-  }
-}
+    )}
+  };
 
 export default App;
