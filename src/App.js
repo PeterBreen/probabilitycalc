30b63a876f81b788053981e2d5fe3339ef0dfcf4
Peter Breen <pbreen@gmail.com>
2017-12-07 14:12:46 -0800

    remember how react handles input values; makes inputs not throw error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import RNGCalc from './Calc.js';
 import './App.css';
+import RNGCalc from './RNGCalc';
 
 class App extends Component {
   render() {
@@ -12,9 +12,8 @@ class App extends Component {
         <p className="App-intro">
           For example, the probability of rolling a 3 on a 6-sided die is 16.67%. If you roll the die 25 times, the probability of rolling a 3 at least once is 98.95%.
         </p>
-        <Calc />
+        <RNGCalc />
       </div>
-
     )}
   };
 
--- src/RNGCalc.js
+++ src/RNGCalc.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
-import './Calc.css';
+import './RNGCalc.css';
 
 class RNGCalc extends Component {
-  render() {
-    return (
-      <div className="RNGCalc">
-      <label for="outcomeProb">Outcome Probability</label>
-      <input id="outcomeProb" type="number" name="outcomeProb" value="0">
 
-      <label for="numAttempts">Number of attempts</label>
-      <input id="numAttempts"> type="number" name="numAttempts" value="0">
-      </div>
-    )}
-  };
+  constructor(props) {
+     super(props);
+     this.state = {
+       outcomeProb: 0,
+       numAttempts: 0
+     };
+     this.handleChange = this.handleChange.bind(this);
+     this.handleSubmit = this.handleSubmit.bind(this);
+   }
+
+   handleChange(event) {
+     this.setState({value: event.target.value});
+   }
+
+   handleSubmit(event) {
+     alert('A name was submitted: ' + this.state.value);
+     event.preventDefault();
+   }
+
+   render() {
+     return (
+       <form onSubmit={this.handleSubmit}>
+         <label>
+           Name: Outcome Probability
+           <input type="number" value={this.state.outcomeProb} onChange={this.handleChange} />
+         </label>
+         <label>
+           Name: Number of Attempts
+           <input type="number" value={this.state.numAttempts} onChange={this.handleChange} />
+         </label>
+         <input type="submit" value="Submit" />
+       </form>
+     );
+   }
+ };
 
 export default RNGCalc;
