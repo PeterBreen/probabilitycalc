b70af56026a21506763ab593c5a1daf1cd554b38
Peter Breen <pbreen@gmail.com>
2018-01-03 09:27:59 -0800

    updates App to include  <Intro /> component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import RNGCalc from './RNGCalc';
+import IntroText from './Intro';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          This is a tool for calculating the probability of getting a certain outcome at least once, within a given number of attempts.
-        </p>
-        <p className="App-intro">
-          For example, the probability of rolling a 3 on a 6-sided die is 16.67%. If you roll the die 25 times, the probability of rolling a 3 at least once is 98.95%.
-        </p>
+        <IntroText />
         <RNGCalc />
       </div>
     )}
