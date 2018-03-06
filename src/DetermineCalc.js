4716865c683df089cd418a0198d546959ff33c0a
Peter Breen <pbreen@gmail.com>
2018-03-05 18:49:59 -0800

    add Determine module

---
+++ src/DetermineCalc.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './DetermineCalc.css';
+
+class DetermineCalc extends Component {
+  render() {
+    return (
+      <div>
+        <p>DetermineCalc section rendering!</p>
+      </div>
+    )}
+  };
+
+export default DetermineCalc;
