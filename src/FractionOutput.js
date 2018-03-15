ff7da9f1c63407f26605cb7c28fca99c9d8b1865
Peter Breen <pbreen@gmail.com>
2018-03-15 10:42:06 -0700

    mapped li results are now keyed uniquely not using index values

--- src/FractionOutput.js
+++ src/FractionOutput.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import './FractionOutput.css';
 
 class FractionOutput extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {
+       keyValue: 0
+     };
+   }
   render() {
+    let listId = 0;
     const numbers = this.props.data;
     const listItems = numbers.map((number) =>
-    <li key={number.toString()}>{number[0]} / {number[1]}</li>
+    <li key={listId++}>{number[0]} / {number[1]}</li>
     );
 
     return (
       <div className="fraction-output">
-      <ul>
-        {listItems}
-      </ul>
+        <ul>
+          {listItems}
+        </ul>
       </div>
     )}
   };
